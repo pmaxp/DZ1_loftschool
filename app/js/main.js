c1a6812732a11043a07c2920313584029f6413a4
p-max-p <anbelmp@gmail.com>
2015-12-10 13:38:48 +0300

    add popup

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,37 @@
-$(document).ready(function() {
-	console.log("Я подключился, Jquary!");
-});
+// $(document).ready(function(){
+//     PopUpHide();
+// });
+// function PopUpShow(){
+//     $("#popup1").show();
+// }
+// function PopUpHide(){
+//     $("#popup1").hide();
+// }
+// Semicolon (;) to ensure closing of earlier scripting
+    // Encapsulation
+    // $ is assigned to jQuery
+    ;(function($) {
+
+         // DOM Ready
+        $(function() {
+
+            // Binding a click event
+            // From jQuery v.1.7.0 use .on() instead of .bind()
+            $('#my-button').bind('click', function(e) {
+
+                // Prevents the default action to be triggered. 
+                e.preventDefault();
+
+                // Triggering bPopup when click event is fired
+                $('#element_to_pop_up').bPopup({
+		            speed: 650,
+           			transition: 'slideDown',
+				    transitionClose: 'slideDown'
+			    });
+            
+		
+            });
+
+        });
+
+    })(jQuery);
